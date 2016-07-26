7697fd0b4846c96aece3c85ae874798b7f864717
officel <nishimuray@gmail.com>
2016-07-26 18:38:50 +0900

    serverspec test

--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -4,3 +4,6 @@ describe command('ls /etc/ansible/roles/role_test') do
   its(:exit_status) { should eq 0 }
 end
 
+describe command('sysctl -en net.ipv4.tcp_tw_recycle') do
+  its(:stdout) { should contain('1') }
+end
