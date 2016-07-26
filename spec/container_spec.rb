95ce87fea76e7a731638f7d4fd06315db39d4e8e
officel <nishimuray@gmail.com>
2016-07-26 18:45:23 +0900

    sysctl option...

--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -4,6 +4,6 @@ describe command('ls /etc/ansible/roles/role_test') do
   its(:exit_status) { should eq 0 }
 end
 
-describe command('sysctl -en net.ipv4.tcp_tw_recycle') do
+describe command('sysctl -e -n net.ipv4.tcp_tw_recycle') do
   its(:stdout) { should contain('1') }
 end
