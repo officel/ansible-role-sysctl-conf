8dfb661545a70f6444b8af8693370d9e0c0429f0
officel <nishimuray@gmail.com>
2016-09-06 08:03:18 +0900

    typo file name..._no

--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -4,7 +4,7 @@ describe command('ls /etc/ansible/roles/role_test') do
   its(:exit_status) { should eq 0 }
 end
 
-describe file('/etc/sysctrl.conf') do
+describe file('/etc/sysctl.conf') do
   it { should contain('1').after('net.ipv4.tcp_tw_recycle') }
   it { should contain('1').after('net.ipv4.tcp_tw_reuse') }
   it { should contain('30').after('net.ipv4.tcp_fin_timeout') }
