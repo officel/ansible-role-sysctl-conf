8d7b414409047eaff24e6d9acee80c391ed34395
officel <nishimuray@gmail.com>
2016-09-06 07:42:08 +0900

    spec...

--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -4,6 +4,9 @@ describe command('ls /etc/ansible/roles/role_test') do
   its(:exit_status) { should eq 0 }
 end
 
-describe command('/bin/cat /etc/sysctrl.conf') do
-  its(:stdout) { should contain('8192').after('net.core.somaxconn') }
+describe file('/etc/sysctrl.conf') do
+  it { should contain('1').after('net.ipv4.tcp_tw_recycle') }
+  it { should contain('1').after('net.ipv4.tcp_tw_reuse') }
+  it { should contain('30').after('net.ipv4.tcp_fin_timeout') }
+  it { should contain('8192').after('net.core.somaxconn') }
 end
